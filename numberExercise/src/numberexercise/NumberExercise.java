faef47a39eff436fcecb603a8f6774afa7adcfae
Rido Le <166928218+Rido9812@users.noreply.github.com>
2024-09-03 22:29:30 +0700

    Add files via upload

---
+++ numberExercise/src/numberexercise/NumberExercise.java
@@ -0,0 +1,12 @@
+
+package numberexercise;
+import java.util.*;
+
+public class NumberExercise {
+     static int[] importData(int[] arrays) {
+        ArrayList<Integer> list = new ArrayList<>();
+    }
+        
+    
+    
+
